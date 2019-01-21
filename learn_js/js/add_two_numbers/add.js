1ba67a994fc61cbdc41e96932c9a1d02eae83a4a
jonathan nshuti <nshutijonathan130@gmail.com>
2019-01-21 21:32:17 +0200

    Modified

---
+++ learn_js/js/add_two_numbers/add.js
@@ -0,0 +1,12 @@
+
+"use strict";
+
+function add() {
+    var x = Number(document.getElementById('x').value);
+    var y = Number(document.getElementById('y').value);
+
+    document.getElementById('result').innerHTML = x+y;
+    
+}
+
+document.getElementById('go').addEventListener('click', add);
